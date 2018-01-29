e7efe9256806e85dbfb7ed3659d8480b3622ab5f
Harsh Kumar <harshkumar101@gmail.com>
2018-01-29 11:46:49 -0800

    run commands of both the models moved to separate run files

---
+++ code/run_oil_spill_model.py
@@ -0,0 +1,3 @@
+import Oil_Spill_Model as OSM
+
+acc_train, acc_val, acc_test = OSM.oil_spill_model("./data")
---
+++ code/run_oil_spill_model_extra_credit.py
@@ -0,0 +1,3 @@
+import Oil_Spill_Model_Extra_Credit as OSMEC
+
+acc_train, acc_val, acc_test = OSMEC.oil_spill_model_extra_credit("./data")
