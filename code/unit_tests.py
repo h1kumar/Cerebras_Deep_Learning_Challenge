08d35c64b964fdaa484f58836de8106a0ee40953
Harsh Kumar <harshkumar101@gmail.com>
2018-01-29 19:55:21 -0800

    unit tests added for sigmoid and sigmoid_derivative

--- code/unit_tests.py
+++ code/unit_tests.py
@@ -1,17 +1,17 @@
 import Oil_Spill_Model as osm
-import Oil_Spill_Model_Extra_Credit as osmec
 
 ## Basic Testing of Relu Functions and their Derivatives
 a,_ = osm.relu(0,0,0)
 assert a == 0
+
 a   = osm.relu_derivative(1,-1)
 assert a == 0
+
 a   = osm.relu_derivative(1,1)
 assert a == 1
 
-a,_ = osmec.relu(0,0,0)
-assert a == 0
-a   = osmec.relu_derivative(1,-1)
-assert a == 0
-a   = osmec.relu_derivative(1,1)
+a,_ = osm.sigmoid([[-1],[0],[1]],[[1,1,1]],[0])
+assert a == 0.5
+
+a = osm.sigmoid_derivative(4,0.5)
 assert a == 1
